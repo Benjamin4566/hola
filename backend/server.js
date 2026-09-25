536b8748cd546138cd3482c74dcc482e45162fb1
Benjamin4566 <ayalabenjamin965@gmail.com>
2026-09-25 19:52:27 -0300

    intento de base de ddatos

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 
@@ -25,6 +26,62 @@ db.connect((err) => {
 
 app.get("/", (req, res) => {
     res.send("Servidor de Biblioteca Virtual funcionando 🚀");
+});
+
+app.post("/registro", async (req, res) => {
+
+    const { usuario, contraseña } = req.body;
+
+    if (!usuario || !contraseña) {
+        return res.status(400).json({
+            mensaje: "Faltan datos"
+        });
+    }
+
+    try {
+
+        const contraseñaEncriptada = await bcrypt.hash(contraseña, 10);
+
+        const sql = `
+            INSERT INTO usuarios (usuario, contraseña)
+            VALUES (?, ?)
+        `;
+
+        db.query(
+            sql,
+            [usuario, contraseñaEncriptada],
+            (err, resultado) => {
+
+                if (err) {
+
+                    if (err.code === "ER_DUP_ENTRY") {
+                        return res.status(400).json({
+                            mensaje: "Ese usuario ya existe"
+                        });
+                    }
+
+                    console.log("Error al registrar:", err.message);
+
+                    return res.status(500).json({
+                        mensaje: "Error al registrar usuario"
+                    });
+                }
+
+                res.json({
+                    mensaje: "Usuario creado correctamente",
+                    id: resultado.insertId
+                });
+            }
+        );
+
+    } catch (error) {
+
+        console.log("Error:", error.message);
+
+        res.status(500).json({
+            mensaje: "Error interno del servidor"
+        });
+    }
 });
 
 app.listen(3000, () => {
